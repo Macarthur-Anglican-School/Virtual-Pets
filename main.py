f64eefb239350aac6546cba80753fb599b39dd16
Israel <100066524+isr431@users.noreply.github.com>
2025-05-14 02:32:59 +0000

    Update

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import random
+
+genders = ["male", "female"]
+
 class Pet():
     BABY = r""" v___v
 (o)_(o)
@@ -13,13 +17,18 @@ class Pet():
  \_________/
   ᗜ      ᗜ"""
 
-    def __init__(self, name, age=0, hunger=5, boredom=3, sleepiness=3):
+    def __init__(self, name, age=0, gender=None, hunger=5, boredom=3, sleepiness=3):
         self.name = name
         self.age = age
         self.hunger = hunger
         self.boredom = boredom
         self.sleepiness = sleepiness
         self.__dead = self.check_death()
+
+        if gender:
+            self.gender = gender
+        else:
+            self.gender = random.choice(genders)
     
     def __str__(self):
         return f"""Name: {self.name}
@@ -55,43 +64,10 @@ Sleepiness: {'●' * self.sleepiness}"""
     def check_death(self):
         return self.boredom >= 10 and self.sleepiness >= 10 and self.hunger >= 10 and self.age >= 15
 
+def reproduce(pet1: Pet, pet2: Pet, name, **kwargs):
+    if pet1.gender != pet2.gender:
+        return Pet(name, **kwargs)
+    else:
+        raise Exception("Homosexual Alert")
+
 bobby = Pet("Bobby")
-print(Pet.ADULT)
-
-####----Task 1----####
-#Set up your pet with the following attributes:
-#name (make this mandatory), age (default:0), hunger (default: 5), boredom (default:3), sleepiness(default:3)
-
-####----Task 2----####
-#instantiate your pet object with the name of your choice
-
-####----Task 3----#### 
-# We need to add the following methods to our Virtual Pet:
-# 1. Feed - which will reduce hunger by 3
-# use a selection to make sure if hunger goes below zero it gets reset to 0 (we don’t want any negative numbers.)
-# 2. Play - which will reduce boredom by 3
-# 3. Sleep - which will reduce sleepiness by 5
-# 4. Wait - which will increase age, and increase hunger, boredom and sleepiness
-# 5. Is_dead - which will check to see if the Pet has hit the thresholds we have set as the
-# maximum possible hunger, boredom and sleepiness
-
-###----Task 4----####
-# Make a new method called check_death() that checks when a pet dies.
-# These are the conditions I have chosen to use to determine if the pet should be dead.
-# (Note: you can change these to make your pet harder or easier to keep alive)
-    # ● Boredom is at 10
-    # ● Sleepiness is at 10
-    # ● Hunger is at 10
-    # ● Age is at a random age between 15 and 20 or more
-
-
-####---Task 5----####
-#make it so that the feed, sleep, play and wait will check if the pet
-#is dead before you upadate those properties.
-
-####---Task 6----####
-#Use Python's predefined __str__ method to produce a string output
-#for your pet. refer to page 4 of the tutorial if you don't know
-#what I'm talking about.
-
-#Go to page 9 of the tutorial to learn how to make the mainline (https://classroom.google.com/w/NzE2NTQ0NzA2MTYx/t/all)
